bc0b7edd23d3ead798615304c0a5c76b8dac3fd4
Ralph Schaefermeier <ralph.schafermeier@gmail.com>
2024-06-06 17:56:11 +0200

    Add identity provider selection

--- src/components/SolidChatSession.tsx
+++ src/components/SolidChatSession.tsx
@@ -1,14 +1,16 @@
 import { Chat } from './Chat';
 import { User } from "@chatscope/use-chat";
 import { useState, useEffect } from "react";
+import Creatable from 'react-select/creatable';
 import { useSession, LoginButton, LogoutButton } from "@inrupt/solid-ui-react";
+import { identityProviderOptions } from './solid-identityproviders';
 
 export const SolidChatSession = ({user}:{user:User}) => {
 
     const { session } = useSession();
 
-    const [idp, setIdp] = useState("https://login.inrupt.com");
-    const [currentUrl, setCurrentUrl] = useState("https://localhost:3000");
+    const [idp, setIdp] = useState<({label:string, value:string}|null)>({ label: identityProviderOptions[0].label, value: identityProviderOptions[0].value });
+    const [currentUrl, setCurrentUrl] = useState(window.location.href);
   
     useEffect(() => {
       setCurrentUrl(window.location.href);
@@ -24,12 +26,20 @@ export const SolidChatSession = ({user}:{user:User}) => {
             <LogoutButton />
             </>
         ) : (
+          <>
+          Please select your identity provider (idp) and log in to (one of) your Solid Pod(s):
+              <Creatable
+                options={identityProviderOptions}
+                value={idp}
+                defaultValue={{ label: identityProviderOptions[0].label, value: identityProviderOptions[0].value }}
+                onChange={selection => setIdp(selection)} />
             <LoginButton
                 authOptions={{ clientName: "Vecomp Chat" }}
-                oidcIssuer={idp}
+                oidcIssuer={idp == null ? "" : idp.value}
                 redirectUrl={currentUrl}
                 onError={console.error}
             />
+          </>
         )}
         </div>
       );
---
+++ src/components/solid-identityproviders.js
@@ -0,0 +1,7 @@
+// Specify preconfigured default identity providers here (we use an editable Selection component,so users can select one from the default providers configured here but will still be able to specify their own provider if it is missing from this config)
+
+export const identityProviderOptions = [
+    {label: "https://solidcommunity.net/", value: "https://solidcommunity.net/"},
+    {label: "https://login.inrupt.com/", value: "https://login.inrupt.com/"},
+    {label: "https://inrupt.net/", value: "https://inrupt.net/"}
+];
